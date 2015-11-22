6010be325408a053fa764b9ea982236d28df4602
mgmt01v <xsx@mgmt01v.home>
2015-11-22 12:50:29 -0500

    initial release, testing

---
+++ test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import asyncio, asyncssh, sys
+
+# 10ms poll cycle
+YIELD_TIMEOUT=0.01
+
+_cmds = ['echo test', 'whoami', 'exit 127']
+_host_dict = {}
+
+def parse_hosts(path='/etc/hosts'):
+    with open(path) as f:
+        hosts = f.readlines()
+    hosts = [line.strip().split() for line in hosts if not line.startswith('#') and line.strip()]
+    return {line[0]: line[1:] for line in hosts}
+
+def ip2host(ip):
+    global _host_dict
+
+    return _host_dict[ip][0]
+
+class MySSHClientSession(asyncssh.SSHClientSession):
+    def connection_made(self, chan):
+        self._chan = chan
+        self.hostname = ip2host(chan.get_extra_info('peername')[0])
+
+    def data_received(self, data, datatype):
+        print('[%s]~' % self.hostname, data, end='')
+
+    def exit_status_received(self, status):
+        if status:
+            print('[%s]! Exited with status %d' % (self.hostname, status), file=sys.stderr)
+        else:
+            print('[%s]- Completed successfully' % self.hostname)
+
+    def connection_lost(self, exc):
+        if exc:
+            print('[%s]! SSH session error: %s' % (self.hostname, str(exc)), file=sys.stderr)
+
+@asyncio.coroutine
+def run_client(host):
+    with (yield from asyncssh.connect(host, known_hosts=None)) as conn:
+        print('[%s]+ Connection initiated' % host)
+        for cmd in _cmds:
+            chan, session = yield from conn.create_session(MySSHClientSession, cmd)
+            print('[%s]* Executing command: %s' % (host, cmd))
+            yield from chan.wait_closed()
+
+@asyncio.coroutine
+def manager(tasks):
+    while tasks:
+        for task in tasks:
+            if task.done():
+                tasks.remove(task)
+            yield from asyncio.sleep(YIELD_TIMEOUT)
+    loop.stop()
+
+
+if __name__ == '__main__':
+    try:
+        _host_dict = parse_hosts()
+
+        tasks = []
+        loop = asyncio.get_event_loop()
+        for host in ['archt01', 'archt02', 'archt03', 'archt04', 'archt05']:
+            tasks.append(asyncio.async(run_client(host)))
+        asyncio.async(manager(tasks))
+        loop.run_forever()
+    except (OSError, asyncssh.Error) as exc:
+        sys.exit('SSH connection failed: ' + str(exc))
