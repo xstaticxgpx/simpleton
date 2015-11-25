6f8fa2fdc6c7f89c7d3a5cf350d0e6d46b487d91
Gabe <optimum.reflex@gmail.com>
2015-11-25 11:28:00 -0500

    updating _argparser.py doc

--- _argparser.py
+++ _argparser.py
@@ -4,7 +4,7 @@ Simpleton's goal is to provide an extremely simple alternative to host/configura
 
 This is accomplished by using the SSH protocol for an agentless design.
 
-As opposed to a forked or threaded model, I'm opting to try and use an asynchronous implementation using `asyncssh`
+As opposed to a fork or thread model, I'm opting to try and use an asynchronous, event oriented implementation using `asyncssh`
 """
 
 import argparse
